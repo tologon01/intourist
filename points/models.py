c63ff3e940c024819529a122b64371fe0349586e
tologon01 <77880104+tologon01@users.noreply.github.com>
2021-07-17 12:18:27 +0300

    add user fk points models

--- points/models.py
+++ points/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.db.models.base import Model
-
+from django.contrib.auth.models import User
 class Points(models.Model):
+    user = models.ForeignKey(
+        to=User, on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     views_count = models.IntegerField(default=0)
