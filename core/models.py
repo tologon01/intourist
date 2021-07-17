b2bcfa5f1f0def4448993a00f3d4b41dc854272f
tologon01 <77880104+tologon01@users.noreply.github.com>
2021-07-17 18:09:55 +0300

    add core profile admin

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import CharField
+from django.db.models.fields.files import ImageField
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        to=User, on_delete=models.CASCADE,
+        related_name='profile'
+    )
+
+    region = models.CharField(max_length=255, choices=(
+        ('B', 'Bishkek'),
+        ('C', 'Chuy'),
+        ('I', 'Issyk-kyl'),
+        ('N', 'Naryn'),
+        ('T', 'Talas'),
+        ('O', 'Osh'),
+        ('D', 'Djalal-Abad'),
+        ('A', 'Batken'),
+    ))
+
+    photo = models.ImageField(
+        upload_to='profile_photo',
+        null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
